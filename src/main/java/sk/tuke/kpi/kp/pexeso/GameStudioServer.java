b3f59f7d274da02cd96aafbfa5f686cb658603e7
Vadym <vadym.khudyna@student.tuke.sk>
2025-06-27 13:14:24 +0200

    Remove redundant Pexoso folder

---
+++ src/main/java/sk/tuke/kpi/kp/pexeso/GameStudioServer.java
@@ -0,0 +1,12 @@
+package sk.tuke.kpi.kp.pexeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameStudioServer {
+    public static void main(String[] args) {
+        SpringApplication.run(GameStudioServer.class, args);
+    }
+}
+git restore --staged Pexoso
